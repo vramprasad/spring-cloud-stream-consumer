d7f667697b782fc9750e43f43dfc7cf335a48a62
Ramprasad V <vrprasad@gmail.com>
2022-05-21 10:23:27 +0530

    Added correlationID

--- src/main/java/com/prasad/ConsumerApp.java
+++ src/main/java/com/prasad/ConsumerApp.java
@@ -1,5 +1,7 @@
 package com.prasad;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -9,10 +11,10 @@ import org.springframework.cloud.stream.messaging.Sink;
 @SpringBootApplication
 @EnableBinding(Sink.class)
 public class ConsumerApp {
-
+    private Logger logger = LoggerFactory.getLogger(ConsumerApp.class);
     @StreamListener("input")
     public void consumeMessage(EnrichmentJob enrichmentJob){
-        System.out.println("Message consumed - " + enrichmentJob.toString());
+        logger.info("Message consumed - " + enrichmentJob.toString());
     }
 
     public static void main(String[] args) {
